9a2144f36208975fd984c6c2d20937663cc021c9
negishitakuro <t_negi@hotmail.com>
2021-07-04 19:06:13 +0900

    BottomNavigationタブ実装

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 
 import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 import 'package:permission_handler/permission_handler.dart';
 import 'package:ring/pages/auth.dart';
 import 'package:ring/pages/call.dart';
+import 'package:ring/utils/settings.dart';
 
 class IndexPage extends StatefulWidget {
   @override
@@ -15,10 +18,12 @@ class IndexPage extends StatefulWidget {
 
 class IndexState extends State<IndexPage> {
   /// create a channelController to retrieve text value
-  final _channelController = TextEditingController();
+  // final _channelController = TextEditingController();
 
   /// if channel textField is validated to have error
-  bool _validateError = false;
+  // bool _validateError = false;
+
+  int _selectedIndex = 0;
 
   ClientRole? _role = ClientRole.Broadcaster;
 
@@ -27,13 +32,53 @@ class IndexState extends State<IndexPage> {
   @override
   void dispose() {
     // dispose input controller
-    _channelController.dispose();
+    // _channelController.dispose();
     super.dispose();
+  }
+
+  Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>> getApiData() async {
+    final snapshot = await FirebaseFirestore.instance.collection('rooms').get();
+    print(snapshot.docs.first.data());
+    return snapshot.docs;
+  }
+
+  void searchRepositories() async {
+    final url =
+        Uri.parse('https://atnd.ak4.jp/api/cooperation/techfirm/stamps');
+
+    final response = await http.post(url, body: {
+      'token': '$akashiToken',
+      'start_date': '20210501000000',
+      'end_date': '20210531000000'
+    });
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    // final response = await http.get(
+    //     'https://api.github.com/search/repositories?q=' +
+    //         searchWord +
+    //         '&sort=stars&order=desc');
+    //
+    //
+    // if (response.statusCode == 200) {
+    //   List<GithubRepository> list = [];
+    //   Map<String, dynamic> decoded = json.decode(response.body);
+    //   for (var item in decoded['items']) {
+    //     list.add(GithubRepository.fromJson(item));
+    //   }
+    //   // return list;
+    // } else {
+    //   throw Exception('Fail to search repository');
+    // }
   }
 
   @override
   Widget build(BuildContext context) {
     final items = ['ログアウト'];
+    searchRepositories();
+    final _pageList = <Widget>[getRoomsPage(), const Center()];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('ルーム選択'),
@@ -65,111 +110,147 @@ class IndexState extends State<IndexPage> {
           )
         ],
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          height: 400,
-          child: Column(
-            children: <Widget>[
-              Row(
+      body: _pageList[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_rounded),
+            label: '通話',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contacts),
+            label: '勤怠',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget getRoomsPage() {
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        height: 700,
+        child: Column(
+          children: <Widget>[
+            // Row(
+            //   children: <Widget>[
+            //     FutureBuilder<
+            //             List<QueryDocumentSnapshot<Map<String, dynamic>>>>(
+            //         future: getApiData(),
+            //         builder: (context, snapshot) {
+            //           if (!snapshot.hasData) {
+            //             return const CircularProgressIndicator();
+            //           }
+            //           if (snapshot.data == null || snapshot.data!.isEmpty) {
+            //             return const Text('No Data');
+            //           } else {
+            //             print(snapshot.data![0].get('name').toString());
+            //             return Expanded(
+            //                 child: ListView.builder(
+            //               shrinkWrap: true,
+            //               physics: NeverScrollableScrollPhysics(),
+            //               itemCount: snapshot.data!.length,
+            //               itemBuilder: (context, index) {
+            //                 return Text(
+            //                     snapshot.data![index].get('name').toString());
+            //               },
+            //             ));
+            //           }
+            //         }),
+            //   ],
+            // ),
+            Column(
+              children: [
+                ListTile(
+                  title: Text(ClientRole.Broadcaster.toString()),
+                  leading: Radio(
+                    value: ClientRole.Broadcaster,
+                    groupValue: _role,
+                    onChanged: (ClientRole? value) {
+                      setState(() {
+                        _role = value;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: Text(ClientRole.Audience.toString()),
+                  leading: Radio(
+                    value: ClientRole.Audience,
+                    groupValue: _role,
+                    onChanged: (ClientRole? value) {
+                      setState(() {
+                        _role = value;
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Row(
                 children: <Widget>[
                   Expanded(
-                      child: TextField(
-                    controller: _channelController,
-                    decoration: InputDecoration(
-                      errorText:
-                          _validateError ? 'Channel name is mandatory' : null,
-                      border: UnderlineInputBorder(
-                        borderSide: BorderSide(width: 1),
-                      ),
-                      hintText: 'Channel name',
-                    ),
-                  ))
-                ],
-              ),
-              Column(
-                children: [
-                  ListTile(
-                    title: Text(ClientRole.Broadcaster.toString()),
-                    leading: Radio(
-                      value: ClientRole.Broadcaster,
-                      groupValue: _role,
-                      onChanged: (ClientRole? value) {
-                        setState(() {
-                          _role = value;
-                        });
-                      },
+                    child: ElevatedButton(
+                      onPressed: onJoin,
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.blueAccent),
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.white)),
+                      child: const Text('Join'),
                     ),
                   ),
-                  ListTile(
-                    title: Text(ClientRole.Audience.toString()),
-                    leading: Radio(
-                      value: ClientRole.Audience,
-                      groupValue: _role,
-                      onChanged: (ClientRole? value) {
-                        setState(() {
-                          _role = value;
-                        });
-                      },
-                    ),
-                  )
+                  // Expanded(
+                  //   child: RaisedButton(
+                  //     onPressed: onJoin,
+                  //     child: Text('Join'),
+                  //     color: Colors.blueAccent,
+                  //     textColor: Colors.white,
+                  //   ),
+                  // )
                 ],
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: onJoin,
-                        child: Text('Join'),
-                        style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.blueAccent),
-                            foregroundColor:
-                                MaterialStateProperty.all(Colors.white)),
-                      ),
-                    ),
-                    // Expanded(
-                    //   child: RaisedButton(
-                    //     onPressed: onJoin,
-                    //     child: Text('Join'),
-                    //     color: Colors.blueAccent,
-                    //     textColor: Colors.white,
-                    //   ),
-                    // )
-                  ],
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
   }
 
+  // タップ時の処理
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   Future<void> onJoin() async {
     // update input validation
-    setState(() {
-      _channelController.text.isEmpty
-          ? _validateError = true
-          : _validateError = false;
-    });
-    if (_channelController.text.isNotEmpty) {
-      // await for camera and mic permissions before pushing video page
-      await _handleCameraAndMic(Permission.camera);
-      await _handleCameraAndMic(Permission.microphone);
-      // push video page with given channel name
-      await Navigator.push(
-        context,
-        MaterialPageRoute<CallPage>(
-          builder: (context) => CallPage(
-            channelName: _channelController.text,
-            role: _role,
-          ),
+    // setState(() {
+    //   _channelController.text.isEmpty
+    //       ? _validateError = true
+    //       : _validateError = false;
+    // });
+    // if (_channelController.text.isNotEmpty) {
+    // await for camera and mic permissions before pushing video page
+    await _handleCameraAndMic(Permission.camera);
+    await _handleCameraAndMic(Permission.microphone);
+    // push video page with given channel name
+    await Navigator.push(
+      context,
+      MaterialPageRoute<CallPage>(
+        builder: (context) => CallPage(
+          channelName: 'test',
+          role: _role,
         ),
-      );
-    }
+      ),
+    );
+    // }
   }
 
   Future<void> _handleCameraAndMic(Permission permission) async {
--- lib/utils/settings.dart
+++ lib/utils/settings.dart
@@ -1,4 +1,6 @@
 /// Define App ID and Token
 const appId = 'facb055a886e4be6a3d459f8d77f169d';
 const token =
-    '006facb055a886e4be6a3d459f8d77f169dIABojpO03Ev0MILhT//3ot8QM5D0We1w7AsSrhPql9J3Cwx+f9gAAAAAEACqPfBqMxLcYAEAAQAyEtxg';
+    '006facb055a886e4be6a3d459f8d77f169dIAAMo+Zs6QTJPZj2MM52Qq5YT9B4r+3i/J9M+LP3BcrDhQx+f9gAAAAAEACqPfBqi6XhYAEAAQCKpeFg';
+
+const akashiToken = '88168929-c543-4d5c-ae57-1c5cc2b9247b';
